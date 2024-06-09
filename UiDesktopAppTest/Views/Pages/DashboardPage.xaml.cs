491efa84ad310898c974b90c52abea1e27ec2661
bcyoo <89197951+bcyoo@users.noreply.github.com>
2024-06-09 12:28:24 +0900

    프로젝트 생성

---
+++ UiDesktopAppTest/Views/Pages/DashboardPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using UiDesktopAppTest.ViewModels.Pages;
+using Wpf.Ui.Controls;
+
+namespace UiDesktopAppTest.Views.Pages
+{
+    public partial class DashboardPage : INavigableView<DashboardViewModel>
+    {
+        public DashboardViewModel ViewModel { get; }
+
+        public DashboardPage(DashboardViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            DataContext = this;
+
+            InitializeComponent();
+        }
+    }
+}
